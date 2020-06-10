52b81bf094e10879f0e7103cfedb471a7667b677
Wayne <blairu92@gmail.com>
2020-06-10 16:22:43 +0100

    Added Document comments to retrieve_page_html

--- config/config.js
+++ config/config.js
@@ -2,14 +2,14 @@ const mongoose = require("mongoose");
 
 //This connects to local mongodb
 //In order to use online, get a mogodb url from mlab and replace here
-const config = mongoose.connect("mongodb://localhost/docker_app", {
+const config = mongoose.connect("mongodb+srv://wayne:wayne@cluster0-l1oed.mongodb.net/webpages?retryWrites=true&w=majority", {
     useNewUrlParser: true,
     useUnifiedTopology: true
 }, (err, success)=>{
     if(err){
         console.log("Could not connect to database")
     }else{
-    console.log("connected database")
+    console.log("Database Connection Successful")
 }
 });
 
--- controllers/retrieve_page_html_controller.js
+++ controllers/retrieve_page_html_controller.js
@@ -1,22 +1,50 @@
+/**
+ * @file 
+ * retrieve_page_html
+ * 
+ * Returns the html content of a specified webpage;
+ * if the request is an internal request, it returns the HTML markup from
+ * the database. if it is an external request it returns the HTML markup
+ * from the url specified. 
+ */
+
+/**
+ * Holds the settings to make this controller functional
+ * 
+ * @namespace
+ */
 const router = require("express").Router(),
             request = require("request"),
             Data = require("../Resources/database/database"),
-            Config = require("../config/config"),
             mongoose = require("mongoose");
 
-
+/**
+ * Makes a GET request to the database or the url specified
+ * and retrieves the HTML markup from the page
+ * 
+ * @constructor
+ * 
+ * @param {URL} url
+ * The request url that will parsed to carry out a GET (internal or external) request 
+ * containing a link to the page to retrieve the markup whether (url or page_id)
+ * 
+ * @param {String} type
+ * The type of GET request being made i.e internal or external
+ * 
+ * 
+ */
 router.get("/retrieve_page_html", (req, res) => {
     //REQUIRED
     // url
     //type
     const url = req.query.url;
-    const type = req.query.type
+    const type = req.query.type;
 
     //for external links
     if (type == "external") {
         request(url, (error, response, body) => {
             if (error)
-                res.send("Something went wrong!!! : " + error)
+                res.send("Something went wrong!!! : " + error);
             res.status(200).json({
                 status: "success",
                 data: response.body
@@ -28,13 +56,22 @@ router.get("/retrieve_page_html", (req, res) => {
             if (error)
                 res.send(error)
             //Then convert to markdown and send to client
-            res.json({
-                body
-        })
-    }
-        )}
-})
+            res.json({body});
+        }
+    )}
+});
 //for handling internal request after user selects page
+
+/**
+ * Retrieves the markup from an internal request made to a 
+ * page stored in the database
+ * 
+ * @param {Number} page_id
+ * Takes the page id as part of the url and makes a POST request to the
+ * database to return that page's markup 
+ * 
+ */
+
 router.post("/retrieve_page_html:page_id", (req, res) => {
     if(mongoose.connection.readyState == 1){
         const page_id = req.params.page_id;
@@ -46,7 +83,7 @@ router.post("/retrieve_page_html:page_id", (req, res) => {
             });
         })
     }
-    res.status(503).json({status: "No database connection established" })
-})
+    res.status(503).json({status: "No database connection established" });
+});
 
 module.exports = router;
