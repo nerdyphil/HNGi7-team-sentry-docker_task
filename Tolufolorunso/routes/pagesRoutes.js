7fdfedc5706d5a727a379208b8a8b457c81fd05d
Tolulope Folorunso <tolufolorunso@yahoo.com>
2020-06-10 11:23:24 +0100

    rectified changes

--- Tolufolorunso/routes/pagesRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const pageController = require("../controllers/pagesController");
-
-router.get("/list_pages", pageController.getListPages);
-router.route("/add_page").post(pageController.postAPage);
-
-module.exports = router;
